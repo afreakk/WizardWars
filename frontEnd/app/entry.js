9fcb68e512a4a519d738ebf713abf0b0ced6eb0b
hans <laderud@hotmail.com>
2018-05-19 16:44:20 +0200

    slim it down

--- frontEnd/app/entry.js
+++ frontEnd/app/entry.js
@@ -8,22 +8,22 @@
  * - Adds WizardWars Screen once loading is complete
  * - Subscribes and Dispatches to AppStore & DOM
  */
-import { utils, Container } from 'pixi.js';
 import './index.html';
-import Renderer from './Renderer/Renderer';
-import { AnimationStore } from './stores/Store';
-import Store from './stores/Store';
-import WizardWars from './screens/WizardWars';
-import Loader from './screens/Loader';
-import ColorFilter from './filters/color/color';
-import { updateFilterColor, updateFilterIsOn } from './stores/AppStore';
-import BG from './displayobjects/Background/soft.jpg';
-import LOGO from './displayobjects/Logo/logo@2x.png';
 
-const renderer = new Renderer(); // an extension of WebGLRenderer which dispatches to RendererStore
+import {Container, utils} from 'pixi.js';
+
+import ColorFilter from './filters/color/color';
+import Renderer from './Renderer/Renderer';
+import WizardWars from './screens/WizardWars';
+import {updateFilterColor, updateFilterIsOn} from './stores/AppStore';
+import {AnimationStore} from './stores/Store';
+import Store from './stores/Store';
+
+const renderer = new Renderer();  // an extension of WebGLRenderer which
+                                  // dispatches to RendererStore
 renderer.autoResize = true;
-const app = new Container(); // Auto scale to screen size, subscribed to RendererStore
-const loader = new Loader(); // Basic Loading screen
+const app =
+    new Container();  // Auto scale to screen size, subscribed to RendererStore
 
 // Controls for filter/DOM Redux example
 const colorOnInput = document.querySelector('#checkbox');
@@ -35,37 +35,27 @@ document.body.appendChild(renderer.view);
 
 // animate loop for render
 AnimationStore.subscribe(() => {
-  renderer.render(app);
+    renderer.render(app);
 });
 
 // Update DOM and App.filters from AppStore
 Store.subscribe(() => {
-  const { color, coloron } = Store.getState().App;
-  colorOnInput.checked = coloron;
-  colorValueInput.value = utils.hex2string(color);
-  colorFilter.color = color;
-  app.filters = coloron ? [colorFilter] : [];
+    const {color, coloron} = Store.getState().App;
+    colorOnInput.checked = coloron;
+    colorValueInput.value = utils.hex2string(color);
+    colorFilter.color = color;
+    app.filters = coloron ? [colorFilter] : [];
 });
 
 // Dispatch from DOM to AppStore
-colorValueInput.addEventListener('change', v =>
-  Store.dispatch(updateFilterColor(v.currentTarget.value))
-);
-colorOnInput.addEventListener('change', v =>
-  Store.dispatch(updateFilterIsOn(v.currentTarget.checked))
-);
+colorValueInput.addEventListener(
+    'change', v => Store.dispatch(updateFilterColor(v.currentTarget.value)));
+colorOnInput.addEventListener(
+    'change', v => Store.dispatch(updateFilterIsOn(v.currentTarget.checked)));
 
-// Add loader to App Display Object and start loading assets
-app.addChild(loader);
-loader.start([BG, LOGO]);
-
-// remove loader then show example once asset loading is complete
-loader.onLoaded(() => {
-  const example = new WizardWars();
-  example.setRenderer(renderer);
-  app.removeChild(loader);
-  app.addChild(example);
-});
+const example = new WizardWars();
+example.setRenderer(renderer);
+app.addChild(example);
 
 // start the render loop
 renderer.start();
